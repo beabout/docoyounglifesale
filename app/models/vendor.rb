4a002417c5454198d4a5263a57fec84a6ccf9630
cbeeb121 <beaboutclayton@gmail.com>
2020-08-07 03:27:26 +0000

    database updates

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -6,6 +6,7 @@ class Vendor < ApplicationRecord
   has_one_attached :avatar
 
   validates :email, presence: true
+  validate :name_or_company_name_are_present
   after_create_commit :send_welcome_email
   
 
@@ -27,4 +28,11 @@ class Vendor < ApplicationRecord
     VendorMailer.welcome(vendor: self).deliver 
   end
 
+  def name_or_company_name_are_present
+    if company_name.blank? && (first_name.blank? && last_name.blank?)
+      errors.add(:company_name, "Must add either a company name or first and last name.") if company_name.blank? 
+      errors.add(:first_name, "Must add either a company name or first and last name.") if first_name.blank? 
+      errors.add(:last_name, "Must add either a company name or first and last name.") if last_name.blank? 
+    end
+  end
 end
