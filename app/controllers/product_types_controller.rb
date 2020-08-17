c1265ffa2faac6acd0bba0dbc6170a881fcf3203
cbeeb121 <beaboutclayton@gmail.com>
2020-08-17 19:18:50 +0000

    add coordinator button

--- app/controllers/product_types_controller.rb
+++ app/controllers/product_types_controller.rb
@@ -2,16 +2,18 @@ class ProductTypesController < ApplicationController
   before_action :load_products, only: [:show]
 
   def show
+    @donated_items_vendor = Vendor.friendly.find('donated-items')
+    @products = Product.where.not(vendor: @donated_items_vendor).by_type(@product_type)
   end
 
   def index
     @product_types = ProductType.all
+    @donated_items_vendor = Vendor.friendly.find('donated-items')
   end
 
   private 
 
   def load_products   
     @product_type = ProductType.friendly.find(params[:id])
-    @products = Product.where.not(vendor: Vendor.friendly.find('donated-items')).by_type(@product_type)
   end
 end
